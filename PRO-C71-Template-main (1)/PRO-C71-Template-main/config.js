50459b5457592401ee03baf79382ff8bd83090dd
zummers <83545395+zummers@users.noreply.github.com>
2022-02-15 11:19:12 +0000

    Add files via upload

---
+++ PRO-C71-Template-main (1)/PRO-C71-Template-main/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+
+
+
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
+
+
